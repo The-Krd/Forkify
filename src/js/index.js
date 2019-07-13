c96b1034879dc2be1181b7c7a3a67598694d3d59
The-Krd <kaerde91@gmail.com>
2019-07-13 22:10:30 +0200

    add first part of presentation for found recipes in UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 /**
  * search object
@@ -10,22 +12,24 @@ const state = {};
 
 const controlSearch = async () => {
     //get query value from view
-    const query = 'cat'
+    const query = searchView.getInput();
 
     if(query){
         //add newsearch objk and add to state
         state.search = new Search(query);
         // prepare UI for rezuullt
+        searchView.clearInput();     
+        searchView.clearResults();  
 
         //search for reciepes
         await state.search.getResults();
 
         // render resurts on UI
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result); 
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 })
