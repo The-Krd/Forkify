83881c3a4c16a7cdbb18b52017b1842b581ec732
The-Krd <kaerde91@gmail.com>
2019-07-13 17:21:14 +0200

    add connection to recipes API

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,17 @@
-// Global app controller
-import x from './test';
-const f = 23
-console.log(`Im imported ${x} rfrom other mmmma -------- ${f} is xxxiess`);
+import axios from 'axios';
+
+async function getResults(query){
+    const key = 'e2e12d80f62b419d4fbfe0b36261b1ad';
+    try {
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+}
+getResults("west");
+
+
+
+
