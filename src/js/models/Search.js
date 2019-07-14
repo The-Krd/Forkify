163265b07764e8e44334481ca3349e85190d0284
The-Krd <kaerde91@gmail.com>
2019-07-14 10:18:37 +0200

    add search title abbreviation

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -5,7 +5,9 @@ export default class Search {
         this.query = query;
     }
     async getResults(){
-        const key = 'e2e12d80f62b419d4fbfe0b36261b1ad';
+        const key = '7f13c47aa1eba5b3a270b794329b7f0c';
+        //e2e12d80f62b419d4fbfe0b36261b1ad   <<max query limit reached
+        //7f13c47aa1eba5b3a270b794329b7f0c   <to change
         try {
             const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,5 +1,6 @@
 export const elements ={
     searchInput: document.querySelector('.search__field'),
     searchForm: document.querySelector('.search'),
-    searchResultsList: document.querySelector('.results__list')
+    searchResultsList: document.querySelector('.results__list'),
+    searchName: document.querySelectorAll('.results__name')
 };
