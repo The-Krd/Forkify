c3303b5e1e87d7d254b79c9d7d38d0755c9b8e94
The-Krd <kaerde91@gmail.com>
2019-07-25 14:55:40 +0200

    add likes storing in localStorage

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export default 'e2e12d80f62b419d4fbfe0b36261b1ad';
+export default '7f13c47aa1eba5b3a270b794329b7f0c';
         //e2e12d80f62b419d4fbfe0b36261b1ad   <<max query limit reached
         //7f13c47aa1eba5b3a270b794329b7f0c   <to change
         //c7ce34bc6520655648df261f8bbf17bc   
--- src/js/index.js
+++ src/js/index.js
@@ -147,9 +147,6 @@ elements.shopping.addEventListener('click', e=> {
  LIKES CONTROLLER
  */
 
- //ffor tests
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
 
 const controlLike = () => {
     if(!state.likes) state.likes = new Likes();
@@ -183,6 +180,20 @@ const controlLike = () => {
     };
     likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
+
+//likes handling with local storage
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    //restore likes
+    state.likes.readStorage();
+
+    //toggle like menu butt
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    //render existing likes
+    state.likes.likes.forEach( like => likesView.renderLike(like));
+});
 
 //recipe button "+" / "-" clicks
 elements.recipe.addEventListener('click', e => {
